b9a2d8b0692885d312456220ed66b8529b02e67f
Sabuj-Chowdhury <csabuj73@gmail.com>
2025-03-08 17:07:55 +0530

    fetch data from the json file

--- client/src/assets/pages/Home.jsx
+++ client/src/assets/pages/Home.jsx
@@ -1,4 +1,20 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const Home = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/stock_market_data.json")
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((error) => console.error("Error fetching JSON:", error));
+  }, []);
+
+  console.log(data);
+
   return (
     <div>
       <p className="text-xl text-pink-500">Hello</p>
